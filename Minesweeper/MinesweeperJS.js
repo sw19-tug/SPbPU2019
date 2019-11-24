27b06f2738c86364a6552e6efabf9df50239e631
DariaKapustina <daskolcova@gmail.com>
2019-11-24 15:52:47 +0300

    [ Minesweeper-3 ] open cells (DK)

--- Minesweeper/MinesweeperJS.js
+++ Minesweeper/MinesweeperJS.js
@@ -1,3 +1,14 @@
 function goToGameField() {
     document.location.href = "gameField.html";
 }
+
+window.onload = function() {
+	var cells = document.querySelectorAll(".cell");
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].onclick = clickCell;
+	}
+}
+
+function clickCell(cell) {
+	this.classList.add('checked');
+}
