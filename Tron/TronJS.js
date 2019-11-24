4ec61e4f32c417d739546b9070b94e34aad0d218
DGSR <hermanislamov@gmail.com>
2019-11-24 19:27:18 +0300

    [Tron-2] added moving cycle (HI)

--- Tron/TronJS.js
+++ Tron/TronJS.js
@@ -1,3 +1,57 @@
-function goToGameField() {
-    document.location.href = "gameField.html";
+let movement;
+player1=document.getElementById("player1");
+
+function init(){
+    player1.style.top='150px';
+    player1.style.left='150px';
 }
+
+window.onload=init;
+
+function setKey(key) {
+    switch (key) {
+        case 'w':
+        case 'ц':
+            player1.style.top=parseInt(player1.style.top)-5+'px';
+            break;
+        case 'd':
+        case 'в':
+            player1.style.left=parseInt(player1.style.left)+5+'px';
+            break;
+        case 's':
+        case 'ы':
+            player1.style.top=parseInt(player1.style.top)+5+'px';
+            break;
+        case 'a':
+        case 'ф':
+            player1.style.left=parseInt(player1.style.left)-5+'px';
+            break;
+        default:
+            break;
+    };
+};
+
+function handleKeyPress(event) {
+    let key = event.key;
+    switch (key) {
+        case 'w':
+        case 'ц':
+        case 'd':
+        case 'ф':
+        case 's':
+        case 'ы':
+        case 'a':
+        case 'в':
+            if(movement!==undefined)
+                clearInterval(movement);
+            movement=setInterval(()=>setKey(key),100);
+            break;
+        case ' ':
+            if(movement!==undefined)
+                clearInterval(movement);
+            break;
+        default:
+            break;
+    };
+};
+document.addEventListener('keydown', handleKeyPress);
