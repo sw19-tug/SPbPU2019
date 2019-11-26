ca0acc6f1783f99b046b7e5864fd86f8b8d60ae6
nikiforovaki <47817257+nikiforovaki@users.noreply.github.com>
2019-11-26 10:50:36 +0300

    [TacTacToe-6] changed modal form (NK, DD)

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -1,3 +1,12 @@
-window.onload = function () {
-    document.querySelector('a[href="#modal"]').click();
+function selectMode(mode) {
+    document.getElementById('modal').style.display = 'none';
+    !!mode ? multiPlayer() : singlePlayer();
+}
+
+function singlePlayer() {
+    alert('You chose to play with a computer!');
+}
+
+function multiPlayer() {
+    alert('You chose to play with a friend!');
 }
