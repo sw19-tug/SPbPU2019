d3e915d08ebfe4c2f4972f622bcd878b7868c594
nikiforovaki <47817257+nikiforovaki@users.noreply.github.com>
2019-11-27 14:23:40 +0300

    [Hangman-11] add placeholders (NK)

--- Hangman/js/hangmanGame.js
+++ Hangman/js/hangmanGame.js
@@ -2,3 +2,31 @@ function guessLetter(){
 	const targetButton = event.target
 	targetButton.setAttribute("disabled", "disabled")
 }
+
+function placeholders() {
+    var words = [
+        "javascript",
+        "monkey",
+        "amazing",
+        "pancake",
+        "galvainze",
+        "cohort",
+        "concatenate",
+        "iteration",
+        "index",
+        "code",
+        "angular",
+        "react",
+        "python"
+    ];
+    // Pick a random word
+    var word = words[Math.floor(Math.random() * words.length)];
+    var wrapper = document.getElementById('wordWrapper');
+    var str = '';
+    for (var i = 0; i < word.length; i++) {
+        str = str + "_ ";
+    }
+    wrapper.textContent = str;
+}
+window.onload = placeholders();
+
