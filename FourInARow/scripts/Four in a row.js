f12f69b9ae530dc679a7c6e52b4f0caf900a3079
yanhvo <34598655+yanhvo@users.noreply.github.com>
2019-11-29 20:20:22 +0300

    [ FourRow-10 ] Added random player starts (YH)
    Added asynchronous loading of the script. Variables are wrapped in a function.

--- FourInARow/scripts/Four in a row.js
+++ FourInARow/scripts/Four in a row.js
@@ -1,61 +1,84 @@
-﻿let fldWidth = 7;
-let fldHeight = 6;
-let cells = [];
-let unlocked = true;
-let flagPlayer = true; // true -> player1; flase -> player2
+﻿
+initializeGame();
 
-let player1, player2;
+function initializeGame() {
+    const fieldWidth = 7;
+    const fieldHeight = 6;
+    let cells = [];
+    let unlocked = true; 
+    let flagPlayer = true; // true -> player1; flase -> player2
 
-[player1, player2] = ['red', 'gold'];
+    let player1, player2; 
 
-document.addEventListener("DOMContentLoaded", addCells);
+    [player1, player2] = getRandomColor();
 
-function addCells() {
-    let field = document.getElementById("field");
+    addCells();
 
-    for (let i = 0; i < fldWidth; i++) {
-        cells[i] = [];
-    }
+    function addCells() {
+        let field = document.getElementById("field");
 
-    for (let j = 0; j < fldHeight; j++) {
-        for (let i = 0; i < fldWidth; i++) {
-            cells[i][j] = document.createElement('div');
-            cells[i][j].className = 'cell';
-            cells[i][j].style.backgroundColor = 'white';
-            cells[i][j].clmn = i;
-            cells[i][j].row = j;
-            cells[i][j].addEventListener('click', addDisc);
-            field.appendChild(cells[i][j]);
+        for (let i = 0; i < fieldWidth; i++) {
+            cells[i] = [];
+            cells[i].occupiedCells = 0;
+        }
+
+        for (let j = 0; j < fieldHeight; j++) {
+            for (let i = 0; i < fieldWidth; i++) {
+                cells[i][j] = document.createElement('div');
+                cells[i][j].className = 'cell';
+                cells[i][j].column = i; 
+                cells[i][j].row = j;
+                cells[i][j].filled = false;
+                cells[i][j].addEventListener('click', makeMove);
+                field.appendChild(cells[i][j]);
+            }
         }
     }
-}
 
-function addDisc() {
-    if (unlocked) {
-        unlocked = false;
-        animation(this.clmn);
+    function makeMove() {
+        if (unlocked && cells[this.column].occupiedCells < fieldHeight) {
+            unlocked = false;
+            animation(this.column);
+        }
     }
-}
 
-function animation(clmn) {
-    let interval = 0;
-    let i = 0;
-    for (i; i < cells[clmn].length; i++) {
-        if (cells[clmn][i].style.backgroundColor === "white") {
-            setTimeout(div => div.style.backgroundColor = player(), interval, cells[clmn][i]);
+    function animation(column) {
+        let interval = 0;
+        let i = 0;
+
+        for (i; i < cells[column].length; i++) {
+            if (cells[column][i].filled) break;
+            setTimeout(curCell => curCell.style.backgroundColor = player(), interval, cells[column][i]);
             interval += 75;
-            setTimeout(div => div.style.backgroundColor = "white", interval, cells[clmn][i]);
+            setTimeout(curCell => curCell.style.backgroundColor = '', interval, cells[column][i]);
         }
-        else break;
+
+        setTimeout(fillCell, interval, cells[column][i - 1]);
     }
-    setTimeout(div => {
-        div.style.backgroundColor = player();
-        div.player1 = flagPlayer;
-        flagPlayer = !flagPlayer;
+
+    function fillCell(cell) {
+        cell.style.backgroundColor = player();
+        cell.player1 = flagPlayer;
+        cell.filled = true;
+        cells[cell.column].occupiedCells++;
         unlocked = true;
-    }, interval, cells[clmn][i - 1]);
+        flagPlayer = !flagPlayer;
+    }
+
+    function player() {
+        return flagPlayer ? player1 : player2;
+    }
+
+    function getRandomColor() {
+        return getRand(0, 1) ? ["gold", "red"] : ["red", "gold"];
+    }
+
+    function getRand(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; 
+    }
 }
 
-function player() {
-    return flagPlayer ? player1 : player2;
-}
+
+
