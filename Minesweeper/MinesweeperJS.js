e5b137af7e6823ab0a44d03530a47e32488964a3
Karina <ikhsanova.k@mai.ru>
2019-11-30 14:24:38 +0300

    [Minesweeper-2] grid (KI)

--- Minesweeper/MinesweeperJS.js
+++ Minesweeper/MinesweeperJS.js
@@ -3,6 +3,18 @@ function goToGameField() {
 }
 
 window.onload = function() {
+	var field = document.getElementsByClassName("field")[0];
+	var size = 8;
+	for (var i = 0; i < size; i++) {
+		var colDiv = document.createElement('div');
+		for (var j = 0; j < size; j++) {
+			var cell = document.createElement('div');
+			cell.classList.add('cell');
+			colDiv.appendChild(cell);
+		}
+		field.appendChild(colDiv);
+	}
+
 	var cells = document.querySelectorAll(".cell");
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].onclick = clickCell;
