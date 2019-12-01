8d03ed28fd2242c1c06cce9ef44bc5184869e28e
nikiforovaki <47817257+nikiforovaki@users.noreply.github.com>
2019-12-01 19:03:25 +0300

    [Hangman-13] show correct guess (NK)

--- Hangman/js/hangmanGame.js
+++ Hangman/js/hangmanGame.js
@@ -1,34 +1,48 @@
 var flgBig = false;
-function guessLetter(){
-	const targetButton = event.target;;
-	targetButton.setAttribute("disabled", "disabled")
+var arrayRes = [];
+var word;
+function guessLetter() {
+    const targetButton = event.target;;
+    targetButton.setAttribute("disabled", "disabled");
+    //getting id letter
+    document.getElementById('letterBank').onclick = function (e) {
+        letter = e.target.getAttribute('id');
+        addLetters(letter);
+    }
 }
 
 function placeholders() {
-    const words=["light","respect","profit","expansion","heat","touch","move","rate","sound",
-        "change","front","talk","relation","learning","print","servant","feeling","insurance",
-        "punishment","iron","vessel","disease","event","price","judge","summer","science",
-        "measure","organization","side","stage","account","javascript","monkey","amazing",
-        "pancake","galvainze","cohort","concatenate","iteration","index","code","angular",
-        "react","python",'good luck',"take care","welcome guest","written word","heavy competition",
-        "the beginning of time","broken heart","seven virtues"]
+    const words = ["light", "respect", "profit", "expansion", "heat", "touch", "move", "rate", "sound",
+        "change", "front", "talk", "relation", "learning", "print", "servant", "feeling", "insurance",
+        "punishment", "iron", "vessel", "disease", "event", "price", "judge", "summer", "science",
+        "measure", "organization", "side", "stage", "account", "javascript", "monkey", "amazing",
+        "pancake", "galvainze", "cohort", "concatenate", "iteration", "index", "code", "angular",
+        "react", "python", 'good luck', "take care", "welcome guest", "written word", "heavy competition",
+        "the beginning of time", "broken heart", "seven virtues"]
     // Pick a random word
-    var word = words[Math.floor(Math.random() * words.length)];
+    word = words[Math.floor(Math.random() * words.length)];
     var wrapper = document.getElementById('wordWrapper');
-    var str = '';
     for (var i = 0; i < word.length; i++) {
-        if(word[i]!=" ")
-        {
-            str = str + "_ ";
+        if (word[i] != " ") {
+            arrayRes[i] = "_";
         }
-        else {str = str + "  ";}
+        else { arrayRes[i] = " "; }
     }
-    wrapper.textContent = str;
+    wrapper.textContent = arrayRes.join(" ");
 }
 
+//adding a letter to placeholder
+function addLetters(letter) {
+    for (var j = 0; j < word.length; j++) {
+        if (word[j] === letter && arrayRes[j] === "_") {
+            arrayRes[j] = letter;
+        }
+    }
+    var wrapper = document.getElementById('wordWrapper');
+    wrapper.textContent = arrayRes.join(" ");
+}
 
- function makeLettersBig(){
-    
+function makeLettersBig() {
     if (flgBig) {
         document.getElementById("wordWrapper").style.fontSize = "50px";
         document.getElementById("letterBank").style.fontSize = "35px";
@@ -42,4 +56,4 @@ function placeholders() {
         flgBig = true;
     }
 }
-window.onload = function() { placeholders(); }
+window.onload = function () { placeholders(); }
