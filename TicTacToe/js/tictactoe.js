da4e85f0717e69a043cf73561f60b7ebcb0fb466
Daria Divenkova <47813973+dardiv@users.noreply.github.com>
2019-12-03 16:34:24 +0300

    [TicTacToe-8] add select sign in computer mode (DD)

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -17,11 +17,14 @@ var player1 = "X",
       dataO = [];
 
 function selectMode(mode) {
-    document.getElementById('modal').style.display = 'none';
+    document.getElementById('modal-menu').style.display = 'none';
     !!mode ? multiPlayer() : singlePlayer();
 }
 
 function singlePlayer() {
+    document.getElementById('modal-sign').style.display = 'inline';
+    document.getElementById("X").addEventListener('click', chooseSign);
+    document.getElementById("O").addEventListener('click', chooseSign);
     startGame();
 }
 
@@ -39,65 +42,72 @@ function startGame() {
 
 function currentStep() {
 	if (this.innerHTML !== '') {
-        return;
-    }
-    var num = +this.getAttribute("id");
+    return;
+  }
+  var num = +this.getAttribute("id");
 	if (!currentPlayer || currentPlayer == player2){
-        currentPlayer = player1;
-        document.getElementById(this.id).className += " cross";
-        this.innerHTML = currentPlayer;
-        dataX.push(num)
+    currentPlayer = player1;
+    document.getElementById(this.id).className += player1 == "X" ? " cross" : " zero";
+    this.innerHTML = currentPlayer;
+    dataX.push(num)
 		document.getElementById('turn').innerHTML = "Turn: Player " + player2;
 	} else {
 		currentPlayer = player2;
-        document.getElementById(this.id).className += " zero";
-        this.innerHTML = currentPlayer;
-        dataO.push(num)
+    document.getElementById(this.id).className += player2 == "O" ? " zero" : " cross";
+    this.innerHTML = currentPlayer;
+    dataO.push(num)
 		document.getElementById('turn').innerHTML = "Turn: Player " + player1;
-    }
-    stepCount++
-    if (
-        (dataO.length > 2 || dataX.length > 2) &&
-        (checkWin(dataO, num) || checkWin(dataX, num))
+  }
+  stepCount++
+  if (
+      (dataO.length > 2 || dataX.length > 2) &&
+      (checkWin(dataO, num) || checkWin(dataX, num))
       ) {
-        for (var i = 0; i < cells.length; i++) {
-            cells[i].removeEventListener("click", currentStep);
-        }       
-        //show resulte here
-        console.log("Win"+ currentPlayer);
-        return
-      }
-      if (stepCount == 9)
-      {
-          console.log("Draw");
-          return
-      }
+    for (var i = 0; i < cells.length; i++) {
+      cells[i].removeEventListener("click", currentStep);
+    }       
+    //show resulte here
+    console.log("Win"+ currentPlayer);
+    return
     }
-  function checkWin(arr, number) {
-     for (var w = 0, wLen = winCombinations.length; w < wLen; w++) {
-      var someWinArr = winCombinations[w],
+  if (stepCount == 9) {
+    console.log("Draw");
+    return
+  }
+}
+
+function checkWin(arr, number) {
+  for (var w = 0, wLen = winCombinations.length; w < wLen; w++) {
+    var someWinArr = winCombinations[w],
         count = 0;
-      if (someWinArr.indexOf(number) !== -1) {
-        for (var k = 0, kLen = someWinArr.length; k < kLen; k++) {
-             if (arr.indexOf(someWinArr[k]) !== -1) {
-            count++;
-            if (count === 3) {
-                drowCells(someWinArr)
-              return true;
-            }
+    if (someWinArr.indexOf(number) !== -1) {
+      for (var k = 0, kLen = someWinArr.length; k < kLen; k++) {
+        if (arr.indexOf(someWinArr[k]) !== -1) {
+          count++;
+          if (count === 3) {
+            drowCells(someWinArr)
+            return true;
           }
         }
-        count = 0;
       }
+      count = 0;
     }
+  }
+}
+
+function drowCells(winArr) {
+  cellsArr = [];
+  winArr.forEach(element => {
+    cellsArr.push(document.getElementById(element))
+  });
+  cellsArr.forEach(element => {
+    element.className += ' winCell'
+  });
  }
- function drowCells(winArr)
- {
-     cellsArr = [];
-     winArr.forEach(element => {
-         cellsArr.push(document.getElementById(element))
-     });
-     cellsArr.forEach(element => {
-        element.className += ' winCell'
-     })
- }
+
+function chooseSign(){
+  player1 = this.getAttribute("id");
+  player2 = player1 == "O" ? "X" : "O";
+  document.getElementById('modal-sign').style.display = 'none';
+  document.getElementById('turn').innerHTML = "Turn: Player " + player1;
+}
