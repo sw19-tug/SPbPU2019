17d71f308a0e83ba7a8801f3497aa4098888ab47
Dmitriy Drobotov <dmitriy.drobotov@studiomobile.ru>
2019-12-05 13:46:38 +0300

    [Minesweeper-4] Implement placing mines on field (DD, DK)

--- Minesweeper/MinesweeperJS.js
+++ Minesweeper/MinesweeperJS.js
@@ -1,4 +1,6 @@
 const fieldDimension = 8;
+const minesCount = 10;
+let minesLocations = [];
 
 function goToGameField() {
     document.location.href = "gameField.html";
@@ -11,6 +13,7 @@ window.onload = function () {
         for (let j = 0; j < fieldDimension; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
+            cell.id = `${i}_${j}`;
             colDiv.appendChild(cell);
         }
         field.appendChild(colDiv);
@@ -23,5 +26,33 @@ window.onload = function () {
 };
 
 function clickCell(cell) {
-	this.classList.add('checked');
+    this.classList.add('checked');
+    console.log(`Clicked on: ${this.id}`);
+    if (minesLocations.length === 0) {
+        initMinesLocations(this.id);
+    } else {
+        if (minesLocations.includes(this.id)) {
+            this.innerHTML = '💣';
+            // TODO: Implement game over
+        }
+    }
+}
+
+function initMinesLocations(clickCoordinate) {
+    minesLocations = [];
+    for (let i = 0; i < minesCount;) {
+        const mineCoordinate = `${getRandomInt(0, fieldDimension - 1)}_${getRandomInt(0, fieldDimension - 1)}`;
+        if (clickCoordinate !== mineCoordinate && !minesLocations.includes(mineCoordinate)) {
+            minesLocations.push(mineCoordinate);
+            i++;
+        }
+    }
+    console.log(`Mines locations: ${minesLocations}`);
+}
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
