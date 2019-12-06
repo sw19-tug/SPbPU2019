aa183f6c6f3bdff4828598e71f4ed5e7b14bbf35
Reut Elizaveta <39130786+outtatime@users.noreply.github.com>
2019-12-06 03:04:55 +0300

    [Invaders-2] add the ability to move the cannon (ER)

--- SpaceInvaders/js/Game.js
+++ SpaceInvaders/js/Game.js
@@ -5,16 +5,24 @@
 */
 
 class Game {
+	
 	constructor() {
-
+		
 	}
 
-	start() {
-        document.getElementById("main_menu").style.display="none";
+	start() {		
+        document.getElementById("main_menu").style.display="none";        
+        this.cannonPosition=0;
+        document.getElementById("cannon").style.left=this.cannonPosition;
         document.getElementById("game_field").style.display="block";
+        this.fieldHeight=document.getElementById("game_field").clientHeight;
+        this.fieldWidth=document.getElementById("game_field").clientWidth;
+        this.cannonWidth=document.getElementById("cannonPic").clientWidth;
+        document.onkeydown = logKey;
     }
 
 	stop() {
 
     }
+    
 }
--- SpaceInvaders/js/Main.js
+++ SpaceInvaders/js/Main.js
@@ -4,3 +4,24 @@ let game = new Game();
 
 let startButton = document.getElementById('start_button');
 startButton.addEventListener('click', () => game.start());
+
+function logKey(e) {
+	pressedKey=`${e.code}`;
+	switch(pressedKey){
+		case 'ArrowRight':
+			moveCannon(1);
+			break;
+		case 'ArrowLeft':
+			moveCannon(-1);
+			break;
+	}
+}
+
+function moveCannon(direction){
+	speed=10;
+	currentPosition=game.cannonPosition+direction*speed;
+	if((Math.abs(currentPosition)+game.cannonWidth/2<(game.fieldWidth-game.cannonWidth/2))){
+		game.cannonPosition=currentPosition;
+		document.getElementById("cannon").style.left=game.cannonPosition+'px';
+	}
+}	
